946e720c4bd58c8d161b28eac8af57ce58a76f3f
= <56308938+C-L-9524085@users.noreply.github.com>
2019-10-12 11:35:52 +0200

    use convertion fns that don't round numbers for accurate recoloring, keep using 'innacurate' one for ranges

--- index.js
+++ index.js
@@ -30,7 +30,11 @@ const vm = new Vue({
 
 				console.log("adding shade", colorProfileSlot, shadeSlot, rgb)
 
-				this.colorProfilesMainColors[colorProfileSlot][shadeSlot] = {rgb, hsv: rgbToHsv(rgb.r, rgb.g, rgb.b)};
+				this.colorProfilesMainColors[colorProfileSlot][shadeSlot] = {
+					rgb,
+					hsv: rgbToHsv(rgb.r, rgb.g, rgb.b),
+					accurateHSV: rgbToHsv_noRounding(rgb.r, rgb.g, rgb.b)
+				};
 			}
 
 			this.$forceUpdate();
@@ -208,7 +212,11 @@ const vm = new Vue({
 					HSVs.push(HSV);
 
 					if (color.main) {
-						this.colorProfilesMainColors[0][iRow] = { hsv: HSV, rgb: {r: color.r, g: color.g, b: color.b} };
+						this.colorProfilesMainColors[0][iRow] = {
+							hsv: HSV,
+							accurateHSV: rgbToHsv_noRounding(color.r, color.g, color.b),
+							rgb: {r: color.r, g: color.g, b: color.b}
+						};
 						HSVMain = HSV;
 						str += `\n\n// ${row.name}\nset_color_profile_slot( 0, ${iRow}, ${color.r}, ${color.g}, ${color.b} );`;
 					}
@@ -220,7 +228,6 @@ const vm = new Vue({
 					const highest = this.calcHSVRange(HSVs, HSVMain);
 
 					str += `\nset_color_profile_slot_range( ${iRow}, ${highest.h + 1}, ${highest.s + 1}, ${highest.v + 1} );`;
-					this.colorProfilesMainColors[0][iRow].hsv = HSVMain;
 					this.ranges[iRow] = {
 						hL: wrap(360, HSVMain.h - highest.h - 1),
 						hH: wrap(360, HSVMain.h + highest.h + 1),
@@ -327,18 +334,22 @@ const vm = new Vue({
 							&& defaultColorForShade.rgb.g === mainColorForShade.rgb.g
 							&& defaultColorForShade.rgb.b === mainColorForShade.rgb.b)
 								return true;
-								
-							const stepHue = getHueDistance(hsv.h, defaultColorForShade.hsv.h); //todo need to handle direction????
-							const steppedHue = hsv.h > mainColorForShade.hsv.h ? mainColorForShade.hsv.h - stepHue : mainColorForShade.hsv.h + stepHue;
-							hsv.h = wrap(360, steppedHue);
 
-							const stepSat = hsv.s - defaultColorForShade.hsv.s;
-							hsv.s = Math.max(0, Math.min(100, mainColorForShade.hsv.s + stepSat));
 
-							const stepVal = hsv.v - defaultColorForShade.hsv.v;
-							hsv.v = Math.max(0, Math.min(100, mainColorForShade.hsv.v + stepVal));
+							const accurateHSV = rgbToHsv_noRounding(r, g, b);
 
-							const shiftedRgb = HSVtoRGB(hsv.h / 360, hsv.s / 100, hsv.v / 100);
+							const stepHue = defaultColorForShade.accurateHSV.h - accurateHSV.h;
+							accurateHSV.h = mainColorForShade.accurateHSV.h - stepHue;
+							if (accurateHSV.h < 0 || accurateHSV.h > 1)
+								accurateHSV.h = wrap(1, accurateHSV.h);
+
+							const stepSat = defaultColorForShade.accurateHSV.s - accurateHSV.s;
+							accurateHSV.s = Math.max(0, Math.min(1, mainColorForShade.accurateHSV.s - stepSat));
+
+							const stepVal = defaultColorForShade.accurateHSV.v - accurateHSV.v;
+							accurateHSV.v = Math.max(0, Math.min(1, mainColorForShade.accurateHSV.v - stepVal));
+
+							const shiftedRgb = hsvToRgb_noRounding(accurateHSV.h, accurateHSV.s, accurateHSV.v);
 							dataArray[i] = shiftedRgb.r;
 							dataArray[i+1] = shiftedRgb.g;
 							dataArray[i+2] = shiftedRgb.b;
@@ -398,22 +409,62 @@ function rgbToHsv(r, g, b) {
 	return {h, s, v};
 }
 
-/* accepts parameters
- * h  Object = {h:x, s:y, v:z}
- * OR 
- * h, s, v
-*/
-function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
-    if (arguments.length === 1) {
-        s = h.s, v = h.v, h = h.h;
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and v in the set [0, 1].
+ * https://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSV representation
+ */
+function rgbToHsv_noRounding(r, g, b){
+    r = r/255, g = g/255, b = b/255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, v = max;
+
+    var d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if(max == min){
+        h = 0; // achromatic
+    }else{
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
     }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
+
+    return {h, s, v};
+}
+
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ * https://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+function hsvToRgb_noRounding(h, s, v){
+    var r, g, b;
+
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+
+    switch(i % 6){
         case 0: r = v, g = t, b = p; break;
         case 1: r = q, g = v, b = p; break;
         case 2: r = p, g = v, b = t; break;
@@ -421,9 +472,6 @@ function HSVtoRGB(h, s, v) {
         case 4: r = t, g = p, b = v; break;
         case 5: r = v, g = p, b = q; break;
     }
-    return {
-        r: Math.round(r * 255),
-        g: Math.round(g * 255),
-        b: Math.round(b * 255)
-    };
+
+    return {r: r * 255, g: g * 255, b: b * 255};
 }
